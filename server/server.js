c5610c0f99f7cd875b7bc5871e8856f8a7281f46
Dani Tradito <datradito@gmail.com>
2020-05-31 18:53:15 -0300

    feature/server created

---
+++ server/server.js
@@ -0,0 +1,11 @@
+path = require('path');
+const express = require('express');
+const app = express();
+const publicPath = path.join(__dirname, '..', 'public');
+const port = process.env.PORT;
+
+app.use(express.static(publicPath));
+
+app.listen(port, () => {
+    console.log('Server is running...')
+});
