ac201f9e6b0dde1806fabf9b5e451c2301977e9f
Dani Tradito <datradito@gmail.com>
2020-05-31 19:04:56 -0300

    fix server

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 path = require('path');
 const express = require('express');
 const app = express();
-const publicPath = path.join(__dirname, '..', 'public');
+//const publicPath = path.join(__dirname, '..', 'public');
 const port = process.env.PORT;
 
-app.use(express.static(publicPath));
+app.use(express.static(path.resolve(__dirname, '../build')));
+//app.use(express.static(publicPath));
 
 app.listen(port, () => {
     console.log('Server is running...')
